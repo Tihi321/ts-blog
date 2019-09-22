05598af9af48ebeeacf495c09359e65842b50e67
Tihomir Selak <tihomir.selak@infinum.hr>
2019-09-22 13:43:15 +0200

    added lottie on listing

--- views/hero/parts/listing.php
+++ views/hero/parts/listing.php
@@ -9,17 +9,23 @@
 
 use TS_Blog\Plugins\Acf\Theme_Options;
 
-$listing_image = get_field( Theme_Options::LISTING_IMAGE, 'option' );
-$listing_tips  = apply_filters( 'tsb_get_random_field', Theme_Options::LISTING_TIPS );
+$listing_type = get_field( Theme_Options::LISTING_HERO_TYPE, 'option' );
+$hero_file    = ( $listing_type === 'image' ) ? get_field( Theme_Options::LISTING_IMAGE, 'option' ) : get_field( Theme_Options::LISTING_LOTTIE, 'option' );
+$listing_tips = apply_filters( 'tsb_get_random_field', Theme_Options::LISTING_TIPS );
 
-$listing_image_url = $listing_image['url'] ?? '';
 
 $class_name = 'hero';
 
+$lottie_class_name = 'hero__listing-lottie';
+
 ?>
-<div class="<?php echo esc_attr( "{$class_name}__background" ); ?>">
-  <div class="<?php echo esc_attr( "{$class_name}__image" ); ?>" style="background-image: url(<?php echo esc_attr( $listing_image_url ); ?>);"></div>
-</div>
+  <?php
+  $hero_temaplate = ( $listing_type === 'image' ) ? locate_template( 'views/hero/parts/image.php' ) : locate_template( 'views/hero/parts/lottie.php' );
+
+  if ( ! empty( $hero_temaplate ) ) {
+    include $hero_temaplate;
+  }
+  ?>
 <?php if ( $listing_tips ) { ?>
     <div class="<?php echo esc_attr( "{$class_name}__description js-{$class_name}-description" ); ?>">
       <div class="<?php echo esc_attr( apply_filters( 'tsb_get_default_class', 'container' ) ); ?>">
